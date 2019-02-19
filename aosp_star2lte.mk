561c4f07fe5fef2a544e4fb09aff7bba31301ef1
Alex <razielrazi3@gmail.com>
2019-02-19 11:39:59 +0200

    add fingerprint override to pass cts
    it's an older code,sir, but it checks out

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -7,3 +7,8 @@ $(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_star2lte
+
+BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:8.0.0/R16NW/G965FXXU1ARCC:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="star2ltexx-user 8.0.0 R16NW G965FXXU1ARCC release-keys"
